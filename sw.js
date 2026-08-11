7d359987313b9ae2e449f02a1b7339a02a51ecb6
tomos1900 <esrevnoc46@gmail.com>
2026-08-11 15:52:29 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+javascriptconst CACHE_NAME = 'planner-cache-v1';
+const urlsToCache = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
